6cf946feca7b8962672c74914b16f6e7141a3081
Alix Taffle <ataffle@gmail.com>
2019-03-13 10:20:52 +0100

    Add Scrap model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_141448) do
+ActiveRecord::Schema.define(version: 2019_03_13_091810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_02_25_141448) do
     t.date "date"
     t.string "url"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scraps", force: :cascade do |t|
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

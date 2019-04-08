e343eed0f3351a90e9f1fe6ed4dbf79f716deac6
Alix Taffle <ataffle@gmail.com>
2019-04-08 15:38:25 +0200

    clean properties index & datables

--- app/javascript/components/datatables.js
+++ app/javascript/components/datatables.js
@@ -1,6 +1,19 @@
 import $ from 'jquery';
 import 'datatables'
 
-$(document).ready( function () {
-    $('#table-index-properties').DataTable();
-} );
+$("#table-index-properties").dataTable( {
+  language: {
+    search: "<i class='fas fa-search'></i>",
+    searchPlaceholder: "e.g. : Lyon ",
+    // lengthMenu: "Afficher _MENU_ résultats par page",
+    // zeroRecords: "Pas d'en-cours",
+    // info: "Page _PAGE_ sur _PAGES_",
+    // infoEmpty: "No records available",
+    // infoFiltered: "(filtré depuis _MAX_ en-cours)",
+    paginate: {
+      previous: "<i class='fas fa-chevron-left'></i>",
+      next: "<i class='fas fa-chevron-right'></i>"
+    }
+  },
+  "dom": '<"top"f>rt<"bottom"lip><"clear">'
+});
